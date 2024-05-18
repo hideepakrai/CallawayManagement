52a7399e151156201859c7cd791aa184e64afccd
coderforchange <upendrarbt@gmail.com>
2024-05-18 22:20:20 +0530

    create a user CallaWayDevProfile

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "callawaybackend50a45f2b": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
