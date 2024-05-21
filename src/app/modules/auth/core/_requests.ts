c529e354f5fbafc14f0172f8657b1517f9c04f97
upendraCoder <148460923+upendraCoder@users.noreply.github.com>
2024-05-21 17:13:20 +0530

    add local and production env files

--- src/app/modules/auth/core/_requests.ts
+++ src/app/modules/auth/core/_requests.ts
@@ -7,7 +7,8 @@ export const GET_USER_BY_ACCESSTOKEN_URL = `${API_URL}/verify_token`;
 export const LOGIN_URL = `${API_URL}/login`;
 export const REGISTER_URL = `${API_URL}/register`;
 export const REQUEST_PASSWORD_URL = `${API_URL}/forgot_password`;
-  export const serverUrl= import.meta.env.VITE_APP_MY_SERVER_URL;
+export const serverUrl= import.meta.env.VITE_APP_MY_SERVER_URL;
+
 // Server should return AuthModel
 export function login(data:{
   // identifier: string,
