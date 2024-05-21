246e4519558cf24b8e2ff8b6d90c7ff0f3bedc8b
upendraCoder <148460923+upendraCoder@users.noreply.github.com>
2024-05-21 17:29:36 +0530

    Update date and time

--- src/_metronic/partials/widgets/tiles/TilesWidget2.tsx
+++ src/_metronic/partials/widgets/tiles/TilesWidget2.tsx
@@ -1,21 +1,37 @@
-
 import clsx from 'clsx'
 import { toAbsoluteUrl } from '../../../helpers'
 import "./TilesWidget2.css";
 import { Link } from 'react-router-dom'
+
 type Props = {
   className?: string
   bgColor?: string
   title?: string
   title2?: string
 }
+
 const TilesWidget2 = ({
   className,
   bgColor = '#202020',
-  // #663259
   title = 'Create SaaS 1',
   title2 = 'Based Reports',
 }: Props) => {
+  const currentDate = new Date();
+  
+  // Get the current day of the week
+  const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  const dayOfWeek = daysOfWeek[currentDate.getDay()];
+  
+  // Get the current month and day
+  const month = currentDate.toLocaleString('default', { month: 'short' });
+  const day = currentDate.getDate();
+  const year = currentDate.getFullYear();
+  
+  // Format the time
+  const hours = currentDate.getHours();
+  const minutes = currentDate.getMinutes();
+  const formattedTime = `${hours % 12 || 12}:${minutes < 10 ? '0' + minutes : minutes} ${hours < 12 ? 'AM' : 'PM'}`;
+
   return (
     <div
       className={clsx('card time-section bgi-no-repeat bgi-size-contain', className)}
@@ -25,43 +41,17 @@ const TilesWidget2 = ({
         // backgroundImage: `url("${toAbsoluteUrl('media/svg/misc/taieri.svg')}")`,
       }}
     >
+      <div className='card-body d-flex flex-column justify-content-center text-center'>
+        <h4 className='text-white fw-bold mb-6 fs-3'>{dayOfWeek}</h4>
+        <h2 className='text-white mb-7 time-date'>
+          {month} <span className='date text-secondary'>{day}</span>, {year}
+        </h2>
 
-
-
-      <div className='card-body d-flex flex-column justify-content-center text-center '>
-        <h4 className='text-white fw-bold mb-6 fs-3 '>Monday</h4>
-
-        <h2 className='text-white  mb-7 time-date'>Dec <span className='date   text-secondary'>10</span>, 2024</h2>
-
-
-        {/* <div className='d-flex'> */}
         <div className='d-flex text-center calendar'>
           <i className="bi bi-table fs-1 text-white mt-3"></i>
-          <h4 className=' text-secondary mb-2  mx-1 pro-time'> 10:25 AM</h4>
+          <h4 className='text-secondary mb-2 mx-1 pro-time'>{formattedTime}</h4>
           <i className="bi bi-clock fs-1 text-secondary mt-3"></i>
         </div>
-        {/* </div> */}
-
-
-
-        {/* <form >
-        <div className="custom-date-picker">
-          <input value="2024-05-19" type="date" id="date-prodect" name="date-prodect" />
-        </div>
-
-        </form> */}
-
-
-        {/* <h2 className='text-white fw-bold mb-5'>
-          {title} <br /> {title2}{' '}
-        </h2> */}
-
-        {/* <div className='m-0'>
-          
-          <Link className={clsx('btn btn-danger fw-semibold px-6 py-3 ')} to={"/profilepage/managerprofile"}>
-          View Order
-            </Link>
-        </div> */}
       </div>
     </div>
   )
