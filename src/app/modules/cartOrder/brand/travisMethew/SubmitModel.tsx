7215de00ce12eef1effa4d8e716bb76480a9f032
designervj <vijendrachoudhary3344@gmail.com>
2024-06-02 22:50:25 +0530

    submit order model design

--- src/app/modules/cartOrder/brand/travisMethew/SubmitModel.tsx
+++ src/app/modules/cartOrder/brand/travisMethew/SubmitModel.tsx
@@ -16,62 +16,79 @@ type Props = {
 const SubmitModel = ({ isSubmit, onOkHandler, handleCancel }: Props) => {
   const dispatch = useDispatch();
   const [notes, setNotes] = useState<string>('');
+  const [isChecked, setIsChecked] = useState<boolean>(false);
   const getCurrentUsers = useSelector(getCurrentUser) as CurentUser;
 
   useEffect(() => {
     const now = new Date();
     const formattedTimestamp = now.toISOString();
-
-   
   }, [notes, getCurrentUsers, dispatch]);
 
-
-  const handleOk=()=>{
+  const handleOk = () => {
     const now = new Date();
     const formattedTimestamp = now.toISOString();
     if (notes !== '' && getCurrentUsers) {
-        const data1 = {
-          message: notes,
-          name: getCurrentUsers?.name,
-          date: formattedTimestamp,
-          user_id: getCurrentUsers?.id,
-          access: 'all',
-          type: 'user',
-        };
-        dispatch(addNote({
-          note: data1,
-        }));
-      
-      } else if (notes === '' && getCurrentUsers) {
-        const data1 = {
-          message: 'Order submitted',
-          name: getCurrentUsers?.name,
-          date: formattedTimestamp,
-          user_id: getCurrentUsers?.id,
-          access: 'all',
-          type: 'system',
-        };
-        dispatch(addNote({
-          note: data1,
-        }));
-      
-      }
-    onOkHandler()
-    setNotes("")
-  }
+      const data1 = {
+        message: notes,
+        name: getCurrentUsers?.name,
+        date: formattedTimestamp,
+        user_id: getCurrentUsers?.id,
+        access: 'all',
+        type: 'user',
+      };
+      dispatch(addNote({
+        note: data1,
+      }));
+    } else if (notes === '' && getCurrentUsers) {
+      const data1 = {
+        message: 'Order submitted',
+        name: getCurrentUsers?.name,
+        date: formattedTimestamp,
+        user_id: getCurrentUsers?.id,
+        access: 'all',
+        type: 'system',
+      };
+      dispatch(addNote({
+        note: data1,
+      }));
+    }
+    onOkHandler();
+    setNotes('');
+    setIsChecked(false); // Reset the checkbox state after submission
+  };
+
+  const handleCheckboxChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setIsChecked(e.target.checked);
+  };
+
   return (
     <div>
-      <Modal className='timeline submit-popup' title="Add Note" open={isSubmit} onOk={handleOk} onCancel={handleCancel}>
-        <div className='row mt-8'>
-          <h4 className='mb-3 fs-4' style={{ fontWeight: '500' }}>Do you want to submit Order</h4>
-          <div className='col-12'>
-            <TextArea
-              rows={5}
-              placeholder="Note"
-              value={notes}
-              onChange={(e) => setNotes(e.target.value)}
+      <Modal className='timeline submit-popup' title="Do you want to submit Order" open={isSubmit} onOk={handleOk} onCancel={handleCancel}>
+        <div className='row mt-8 '>
+          {/* <h4 className='mb-3 fs-2 text-black' style={{ fontWeight: '500' }}>Do you want to submit Order</h4> */}
+          <div className="form-check form-check-custom form-check-solid mx-3 pt-2">
+            <input
+              className="form-check-input submit-order"
+              type="checkbox"
+              value=""
+              id="flexCheckDefault"
+              checked={isChecked}
+              onChange={handleCheckboxChange}
             />
+            <label className="form-check-label fs-4 text-gray-700" style={{ fontWeight: "500" }}>
+              Add Note
+            </label>
           </div>
+          {isChecked && (
+            <div className='col-12 mt-4'>
+              <TextArea
+                rows={5}
+                placeholder="Note"
+                value={notes}
+                onChange={(e) => setNotes(e.target.value)}
+              />
+            </div>
+          )}
         </div>
       </Modal>
     </div>
