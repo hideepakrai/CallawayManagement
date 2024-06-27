74b89c2fc8daf8e22a39e42f955de85b58a308b6
designervj <vijendrachoudhary3344@gmail.com>
2024-06-27 14:40:17 +0530

    update code

--- src/app/modules/brands/ogio/excel/importExcel/ImportAllOgioProduct.tsx
+++ src/app/modules/brands/ogio/excel/importExcel/ImportAllOgioProduct.tsx
@@ -250,6 +250,7 @@ const ImportAllOgioProduct = ({ onClose, isImport, allOgioData }: Props) => {
           <p className="ant-upload-drag-icon">
             <InboxOutlined />
           </p>
+          
           <p className="ant-upload-text">
             Click or drag file to this area to upload
           </p>
